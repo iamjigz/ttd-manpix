845532ce6a4d780c0ded50cd7858a2a8049d4b81
iamjigz <jigzimon@gmail.com>
2019-02-19 21:01:27 +0800

    Add prettier to lint task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,36 @@
 module.exports = function(grunt) {
-
   // Update this variable if you don't want or can't serve on localhost
   var hostname = 'localhost';
 
   var PORT = {
-    PROD : 9001,
-    DEV : 9901,
-    TEST : 9991
+    PROD: 9001,
+    DEV: 9901,
+    TEST: 9991,
   };
 
   // create a version based on the build timestamp
   var dateFormat = require('dateformat');
-  var version = '-' + dateFormat(new Date(), "yyyy-mm-dd-hh-MM");
+  var version = '-' + dateFormat(new Date(), 'yyyy-mm-dd-hh-MM');
   var releaseVersion = require('./package.json').version;
 
   /**
    * Helper to prefix all strings in provided array with the provided path
    */
-  var prefixPaths = function (paths, prefix) {
-    return paths.map(function (path) {
+  var prefixPaths = function(paths, prefix) {
+    return paths.map(function(path) {
       return prefix + path;
     });
   };
 
   // get the list of scripts paths to include
   var scriptPaths = require('./src/piskel-script-list.js').scripts;
-  var piskelScripts = prefixPaths(scriptPaths, "src/").filter(function (path) {
+  var piskelScripts = prefixPaths(scriptPaths, 'src/').filter(function(path) {
     return path.indexOf('devtools') === -1;
   });
 
   // get the list of styles paths to include
   var stylePaths = require('./src/piskel-style-list.js').styles;
-  var piskelStyles = prefixPaths(stylePaths, "src/");
+  var piskelStyles = prefixPaths(stylePaths, 'src/');
 
   // Casper JS tests
   var casperjsOptions = [
@@ -45,49 +44,71 @@ module.exports = function(grunt) {
   ];
 
   var integrationTestPaths = require('./test/casperjs/integration/IntegrationSuite.js').tests;
-  var integrationTests = prefixPaths(integrationTestPaths, "test/casperjs/integration/");
+  var integrationTests = prefixPaths(integrationTestPaths, 'test/casperjs/integration/');
 
-  var getConnectConfig = function (base, port, host, open) {
+  var getConnectConfig = function(base, port, host, open) {
     return {
       options: {
         port: port,
-        hostname : host,
+        hostname: host,
         base: base,
-        open: open
-      }
+        open: open,
+      },
     };
   };
 
   // load all grunt tasks
   require('load-grunt-tasks')(grunt);
 
+  grunt.loadNpmTasks('grunt-prettier');
+
   grunt.initConfig({
     clean: {
       all: ['dest', 'src/img/icons.png', 'src/css/icons.css'],
       prod: ['dest/prod', 'dest/tmp'],
       desktop: ['dest/desktop', 'dest/tmp'],
-      dev: ['dest/dev', 'dest/tmp']
+      dev: ['dest/dev', 'dest/tmp'],
     },
 
     /**
      * STYLE CHECKS
      */
 
-    leadingIndent : {
+    leadingIndent: {
       options: {
-        indentation : "spaces"
+        indentation: 'spaces',
       },
-      css : ['src/css/**/*.css']
+      css: ['src/css/**/*.css'],
     },
 
     eslint: {
+      options: {
+        fix: true // this will get params from the flags
+      },
       files: [
         // Includes
         'src/js/**/*.js',
         // Exludes
         // TODO: remove this (for now we still get warnings from the lib folder)
-        '!src/js/**/lib/**/*.js'
-      ]
+        '!src/js/**/lib/**/*.js',
+      ],
+    },
+
+    prettier: {
+      options: {
+        // Task-specific options go here.
+        tabWidth: 1,
+        useTabs: true,
+        singleQuote: true,
+        trailingComma: 'es5',
+        bracketSpacing: true,
+        endOfLine: 'auto',
+        progress: true, // By default, a progress bar is not shown. You can opt into this behavior by passing true.
+      },
+      files: {
+        // Target-specific file lists and/or options go here.
+        'src/js/**/*.js': 'src/js/**/*.js',
+      },
     },
 
     /**
@@ -97,7 +118,12 @@ module.exports = function(grunt) {
     connect: {
       prod: getConnectConfig('dest/prod', PORT.PROD, hostname, true),
       test: getConnectConfig(['dest/dev', 'test'], PORT.TEST, hostname, false),
-      dev: getConnectConfig(['dest/dev', 'test'], PORT.DEV, hostname, 'http://' + hostname + ':' + PORT.DEV + '/?debug')
+      dev: getConnectConfig(
+        ['dest/dev', 'test'],
+        PORT.DEV,
+        hostname,
+        'http://' + hostname + ':' + PORT.DEV + '/?debug'
+      ),
     },
 
     watch: {
@@ -105,132 +131,139 @@ module.exports = function(grunt) {
         files: ['src/**/*.*'],
         tasks: ['build'],
         options: {
-          spawn: false
-        }
+          spawn: false,
+        },
       },
       dev: {
         files: ['src/**/*.*'],
         tasks: ['build-dev'],
         options: {
-          spawn: false
-        }
-      }
+          spawn: false,
+        },
+      },
     },
 
     /**
      * BUILD STEPS
      */
 
-    sprite:{
-      all : {
+    sprite: {
+      all: {
         src: 'src/img/icons/**/*.png',
         retinaSrcFilter: 'src/img/icons/**/*@2x.png',
         dest: 'src/img/icons.png',
         retinaDest: 'src/img/icons@2x.png',
-        destCss: 'src/css/icons.css'
-      }
+        destCss: 'src/css/icons.css',
+      },
     },
 
-    concat : {
-      js : {
-        options : {
-          separator : ';'
+    concat: {
+      js: {
+        options: {
+          separator: ';',
         },
-        src : piskelScripts,
-        dest : 'dest/prod/js/piskel-packaged' + version + '.js'
+        src: piskelScripts,
+        dest: 'dest/prod/js/piskel-packaged' + version + '.js',
       },
-      css : {
-        src : piskelStyles,
-        dest : 'dest/tmp/css/piskel-style-packaged' + version + '.css'
-      }
+      css: {
+        src: piskelStyles,
+        dest: 'dest/tmp/css/piskel-style-packaged' + version + '.css',
+      },
     },
 
-    uglify : {
-      options : {
-        mangle : true
+    uglify: {
+      options: {
+        mangle: true,
       },
-      js : {
-        files : {
-          'dest/tmp/js/piskel-packaged-min.js' : ['dest/prod/js/piskel-packaged' + version + '.js']
-        }
-      }
+      js: {
+        files: {
+          'dest/tmp/js/piskel-packaged-min.js': ['dest/prod/js/piskel-packaged' + version + '.js'],
+        },
+      },
     },
 
     includereplace: {
       all: {
         src: 'src/index.html',
         dest: 'dest/tmp/index.html',
-        options : {
-          globals : {
-            'version' : version,
-            'releaseVersion' : releaseVersion
-          }
-        }
-      }
+        options: {
+          globals: {
+            version: version,
+            releaseVersion: releaseVersion,
+          },
+        },
+      },
     },
 
     replace: {
       // main-partial.html is used when embedded in piskelapp.com
       mainPartial: {
         options: {
-          patterns: [{
+          patterns: [
+            {
               match: /^(.|[\r\n])*<!--body-main-start-->/,
-              replacement: "{% raw %}",
-              description : "Remove everything before body-main-start comment"
-            },{
+              replacement: '{% raw %}',
+              description: 'Remove everything before body-main-start comment',
+            },
+            {
               match: /<!--body-main-end-->(.|[\r\n])*$/,
-              replacement: "{% endraw %}",
-              description : "Remove everything after body-main-end comment"
-            },{
+              replacement: '{% endraw %}',
+              description: 'Remove everything after body-main-end comment',
+            },
+            {
               match: /([\r\n])  /g,
-              replacement: "$1",
-              description : "Decrease indentation by one"
-            }
-          ]
+              replacement: '$1',
+              description: 'Decrease indentation by one',
+            },
+          ],
         },
         files: [
           // src/index.html should already have been moved by the includereplace task
-          {src: ['dest/tmp/index.html'], dest: 'dest/prod/piskelapp-partials/main-partial.html'}
-        ]
+          { src: ['dest/tmp/index.html'], dest: 'dest/prod/piskelapp-partials/main-partial.html' },
+        ],
       },
 
       css: {
         options: {
-          patterns: [{
-            match: /var\(--highlight-color\)/g,
-            replacement: "gold",
-          }]
+          patterns: [
+            {
+              match: /var\(--highlight-color\)/g,
+              replacement: 'gold',
+            },
+          ],
         },
-        files: [{
-          src: ['dest/tmp/css/piskel-style-packaged' + version + '.css'],
-          dest: 'dest/prod/css/piskel-style-packaged' + version + '.css'
-        }]
-      }
+        files: [
+          {
+            src: ['dest/tmp/css/piskel-style-packaged' + version + '.css'],
+            dest: 'dest/prod/css/piskel-style-packaged' + version + '.css',
+          },
+        ],
+      },
     },
 
     copy: {
       prod: {
         files: [
           // dest/js/piskel-packaged-min.js should have been created by the uglify task
-          {src: ['dest/tmp/js/piskel-packaged-min.js'], dest: 'dest/prod/js/piskel-packaged-min' + version + '.js'},
-          {src: ['dest/tmp/index.html'], dest: 'dest/prod/index.html'},
-          {src: ['src/logo.png'], dest: 'dest/prod/logo.png'},
-          {src: ['src/js/lib/gif/gif.ie.worker.js'], dest: 'dest/prod/js/lib/gif/gif.ie.worker.js'},
-          {expand: true, src: ['img/**'], cwd: 'src/', dest: 'dest/prod/', filter: 'isFile'},
-          {expand: true, src: ['css/fonts/**'], cwd: 'src/', dest: 'dest/prod/', filter: 'isFile'}
-        ]
+          { src: ['dest/tmp/js/piskel-packaged-min.js'], dest: 'dest/prod/js/piskel-packaged-min' + version + '.js' },
+          { src: ['dest/tmp/index.html'], dest: 'dest/prod/index.html' },
+          { src: ['src/logo.png'], dest: 'dest/prod/logo.png' },
+          { src: ['src/js/lib/gif/gif.ie.worker.js'], dest: 'dest/prod/js/lib/gif/gif.ie.worker.js' },
+          { expand: true, src: ['img/**'], cwd: 'src/', dest: 'dest/prod/', filter: 'isFile' },
+          { expand: true, src: ['css/fonts/**'], cwd: 'src/', dest: 'dest/prod/', filter: 'isFile' },
+        ],
       },
       dev: {
         files: [
           // in dev copy everything to dest/dev
-          {src: ['dest/tmp/index.html'], dest: 'dest/dev/index.html'},
-          {src: ['src/piskel-script-list.js'], dest: 'dest/dev/piskel-script-list.js'},
-          {src: ['src/piskel-style-list.js'], dest: 'dest/dev/piskel-style-list.js'},
-          {expand: true, src: ['js/**'], cwd: 'src/', dest: 'dest/dev/', filter: 'isFile'},
-          {expand: true, src: ['css/**'], cwd: 'src/', dest: 'dest/dev/', filter: 'isFile'},
-          {expand: true, src: ['img/**'], cwd: 'src/', dest: 'dest/dev/', filter: 'isFile'},
-        ]
-      }
+          { src: ['dest/tmp/index.html'], dest: 'dest/dev/index.html' },
+          { src: ['src/piskel-script-list.js'], dest: 'dest/dev/piskel-script-list.js' },
+          { src: ['src/piskel-style-list.js'], dest: 'dest/dev/piskel-style-list.js' },
+          { expand: true, src: ['js/**'], cwd: 'src/', dest: 'dest/dev/', filter: 'isFile' },
+          { expand: true, src: ['css/**'], cwd: 'src/', dest: 'dest/dev/', filter: 'isFile' },
+          { expand: true, src: ['img/**'], cwd: 'src/', dest: 'dest/dev/', filter: 'isFile' },
+        ],
+      },
     },
 
     /**
@@ -239,27 +272,27 @@ module.exports = function(grunt) {
 
     karma: {
       unit: {
-        configFile: 'karma.conf.js'
-      }
+        configFile: 'karma.conf.js',
+      },
     },
 
-    casperjs : {
-      drawing : {
-        files : {
-          src: ['test/casperjs/DrawingTest.js']
+    casperjs: {
+      drawing: {
+        files: {
+          src: ['test/casperjs/DrawingTest.js'],
         },
-        options : {
-          casperjsOptions: casperjsOptions
-        }
+        options: {
+          casperjsOptions: casperjsOptions,
+        },
       },
-      integration : {
-        files : {
-          src: integrationTests
+      integration: {
+        files: {
+          src: integrationTests,
         },
-        options : {
-          casperjsOptions: casperjsOptions
-        }
-      }
+        options: {
+          casperjsOptions: casperjsOptions,
+        },
+      },
     },
 
     /**
@@ -267,44 +300,44 @@ module.exports = function(grunt) {
      */
 
     nwjs: {
-      windows : {
+      windows: {
         options: {
           downloadUrl: 'https://dl.nwjs.io/',
-          version : "0.19.4",
+          version: '0.19.4',
           build_dir: './dest/desktop/', // destination folder of releases.
           win: true,
           linux32: true,
           linux64: true,
-          flavor: "normal",
+          flavor: 'normal',
         },
-        src: ['./dest/prod/**/*', "./package.json", "!./dest/desktop/"]
+        src: ['./dest/prod/**/*', './package.json', '!./dest/desktop/'],
       },
-      macos : {
+      macos: {
         options: {
           downloadUrl: 'https://dl.nwjs.io/',
           osx64: true,
-          version : "0.19.4",
+          version: '0.19.4',
           build_dir: './dest/desktop/',
-          flavor: "normal",
+          flavor: 'normal',
         },
-        src: ['./dest/prod/**/*', "./package.json", "!./dest/desktop/"]
+        src: ['./dest/prod/**/*', './package.json', '!./dest/desktop/'],
       },
-      macos_old : {
+      macos_old: {
         options: {
           downloadUrl: 'https://dl.nwjs.io/',
           osx64: true,
-          version : "0.12.3",
+          version: '0.12.3',
           build_dir: './dest/desktop/old',
-          flavor: "normal",
+          flavor: 'normal',
         },
-        src: ['./dest/prod/**/*', "./package.json", "!./dest/desktop/"]
-      }
-    }
+        src: ['./dest/prod/**/*', './package.json', '!./dest/desktop/'],
+      },
+    },
   });
 
   // TEST TASKS
   // Run linting
-  grunt.registerTask('lint', ['eslint', 'leadingIndent:css']);
+  grunt.registerTask('lint', ['prettier', 'eslint', 'leadingIndent:css']);
   // Run unit-tests
   grunt.registerTask('unit-test', ['karma']);
   // Run integration tests
@@ -312,10 +345,23 @@ module.exports = function(grunt) {
   // Run drawing tests
   grunt.registerTask('drawing-test', ['build-dev', 'connect:test', 'casperjs:drawing']);
   // Run linting, unit tests, drawing tests and integration tests
-  grunt.registerTask('test', ['lint', 'unit-test', 'build-dev', 'connect:test', 'casperjs:drawing', 'casperjs:integration']);
+  grunt.registerTask('test', [
+    'lint',
+    'unit-test',
+    'build-dev',
+    'connect:test',
+    'casperjs:drawing',
+    'casperjs:integration',
+  ]);
 
   // Run the tests, even if the linting fails
-  grunt.registerTask('test-nolint', ['unit-test', 'build-dev', 'connect:test', 'casperjs:drawing', 'casperjs:integration']);
+  grunt.registerTask('test-nolint', [
+    'unit-test',
+    'build-dev',
+    'connect:test',
+    'casperjs:drawing',
+    'casperjs:integration',
+  ]);
 
   // Used by optional precommit hook
   grunt.registerTask('precommit', ['test']);
@@ -323,8 +369,16 @@ module.exports = function(grunt) {
   // BUILD TASKS
   grunt.registerTask('build-index.html', ['includereplace']);
   grunt.registerTask('merge-statics', ['concat:js', 'concat:css', 'uglify']);
-  grunt.registerTask('build',  ['clean:prod', 'sprite', 'merge-statics', 'build-index.html', 'replace:mainPartial', 'replace:css', 'copy:prod']);
-  grunt.registerTask('build-dev',  ['clean:dev', 'sprite', 'build-index.html', 'copy:dev']);
+  grunt.registerTask('build', [
+    'clean:prod',
+    'sprite',
+    'merge-statics',
+    'build-index.html',
+    'replace:mainPartial',
+    'replace:css',
+    'copy:prod',
+  ]);
+  grunt.registerTask('build-dev', ['clean:dev', 'sprite', 'build-index.html', 'copy:dev']);
   grunt.registerTask('desktop', ['clean:desktop', 'default', 'nwjs:windows']);
   grunt.registerTask('desktop-mac', ['clean:desktop', 'default', 'nwjs:macos']);
   grunt.registerTask('desktop-mac-old', ['clean:desktop', 'default', 'replace:desktop', 'nwjs:macos_old']);
